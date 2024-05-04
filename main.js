d1e95712379cf10f4b4a028db88f633ffc13f6aa
okaryoh <okatyan1997@gmail.com>
2024-05-04 17:36:31 +0900

    最初のページ

---
+++ main.js
@@ -0,0 +1,24 @@
+const headerHtml = `
+<header>
+<div class="headerTitle">
+    <div class="title">
+        <h1><a href="index.html">おかだのページ</a></h1>
+    </div>
+    <div class="link">
+        <a href="https://www.facebook.com/profile.php?id=100015394358438" target="_blank" rel="noreferrer noopener">
+            <i class="fa-brands fa-facebook" style="font-size: 24px; color: #0B0A08"></i>
+        </a>
+    </div>
+</div>
+</header>
+<nav>
+<ul>
+    <li><a href="khs-web-pj.html">釜高祭WebページPJ</a></li>
+    <li><a href="info-1.html">情報I</a></li>
+    <li><a href="self-introduction.html">自己紹介</a></li>
+    <li><a href="link.html">おすすめリンク集</a></li>
+</ul>
+</nav>
+`
+
+document.getElementById("header").innerHTML = headerHtml;
