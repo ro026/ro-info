0358e66a35013671498410d7bb51f8f5e12a464a
ro026 <o.r1997ti@gmail.com>
2024-06-26 08:26:37 +0900

    将来はパンくずリストになるところに、一つ前に戻るボタンを設置

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
     const topPageNum = paths.indexOf('ro-info');
     const nowPath = paths[topPageNum + 1];
     const topAbsPath = "../".repeat(paths.length - paths.indexOf('ro-info') - 2 /** 配列番号が0で始まる + 最後のパスはファイル名であるはず */)
+    const isTop = topAbsPath === "";
 
     const menus = [
         { path: "khs-web-pj", name: "釜高祭WebページPJ" },
@@ -20,24 +21,32 @@
 
     const headerHtml = `
 <header>
-<div class="headerTitle">
-    <div class="title">
-        <h1><a href="${topAbsPath}index.html">おかだのページ</a></h1>
+    <div class="headerContents">
+        <div class="headerTitle">
+            <div class="title">
+                <h1><a href="${topAbsPath}index.html">おかだのページ</a></h1>
+            </div>
+            <div class="link">
+                <a href="https://www.facebook.com/profile.php?id=100015394358438" target="_blank" rel="noreferrer noopener">
+                    <i class="fa-brands fa-facebook" style="font-size: 24px; color: #0B0A08"></i>
+                </a>
+            </div>
+        </div>
+        <nav>
+            <ul>${menusHTML}</ul>
+        </nav>
     </div>
-    <div class="link">
-        <a href="https://www.facebook.com/profile.php?id=100015394358438" target="_blank" rel="noreferrer noopener">
-            <i class="fa-brands fa-facebook" style="font-size: 24px; color: #0B0A08"></i>
-        </a>
-    </div>
-</div>
+    <div class="breadcrumb"></div>
 </header>
-<nav>
-<ul>${menusHTML}</ul>
-</nav>
 `
 
-    document.getElementById("header").innerHTML = headerHtml;
+    const headerElement = document.getElementById("header")
+    headerElement.innerHTML = headerHtml;
 
+    const breadcrumbElement = headerElement.querySelector(".breadcrumb")
+    if (!isTop) {
+        breadcrumbElement.innerHTML = `<a href="../index.html">１つ前へ戻る</a>`
+    }
 })()
 
 // グローバル変数
