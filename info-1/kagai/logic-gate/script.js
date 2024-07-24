8bdd86debb64820ca6ba7a62d679a07a79ea0c94
ro026 <o.r1997ti@gmail.com>
2024-07-25 04:26:00 +0900

    AND回路とOR回路に対応する

--- info-1/kagai/logic-gate/script.js
+++ info-1/kagai/logic-gate/script.js
@@ -55,6 +55,9 @@ if (match) {
     logicTitleEle.textContent = titles[currentNumber - 1].title;
 }
 
+/**
+ * スイッチの画像が変わった時の処理
+ */
 function toggleImage(element) {
     const img = element.querySelector('img');
     img.classList.toggle('flipped');
@@ -63,9 +66,74 @@ function toggleImage(element) {
 
 
     // 親要素を取得
-    const parentContainer = element.closest('.logic-gate');
+    let parentContainer = element.closest('.logic-gate');
+    if (parentContainer !== null) {
+        // 入力値が1つ
+        toggleLight(parentContainer);
+    } else {
+        // 入力値が2つ
+        parentContainer = element.closest('.logic-gate-2');
+        const inputsSelector = parentContainer.querySelectorAll(".input-val");
+        const inputs = [];
+        inputsSelector.forEach((input) => {
+            inputs.push(input.textContent);
+        })
+        const selectSelector = parentContainer.querySelector("#gate-selector");
+        const logic = selectSelector.value
+
+        const lightVal = parentContainer.querySelector('.light-val');
+        const oldValue = lightVal.textContent.substring(0, 1);
+
+        changeLight(inputs[0], inputs[1], logic, oldValue, parentContainer);
+    }
+}
+
+/**
+ * 論理回路を変えた時に、論理回路の画像を変える
+ */
+function changeLogic(element) {
+    const logic = element.value;
+    const parentContainer = element.closest('.logic-gate-2');
+    const logicImg = parentContainer.querySelector(".div12 img");
+    logicImg.src = `${logic}.png`;
+
+    const inputsSelector = parentContainer.querySelectorAll(".input-val");
+    const inputs = [];
+    inputsSelector.forEach((input) => {
+        inputs.push(input.textContent);
+    })
+    const lightVal = parentContainer.querySelector('.light-val');
+    const oldValue = lightVal.textContent.substring(0, 1);
+    changeLight(inputs[0], inputs[1], logic, oldValue, parentContainer);
+}
+
+/**
+ * ライトの点灯を反転させる
+ */
+function toggleLight(parentContainer) {
     const lightImg = parentContainer.querySelector(".light");
     lightImg.src = getFileNameFromURL(lightImg.src) === "denkyuu_off.png" ? "denkyuu_on.png" : "denkyuu_off.png";
     const lightVal = parentContainer.querySelector('.light-val');
     lightVal.textContent = lightVal.textContent === '1(点灯)' ? '0(消灯)' : '1(点灯)';
+}
+
+/**
+ * input1, input2 は入力値
+ * logicは論理回路
+ * oldValueは、値が変わっていたらトグルする
+ */
+function changeLight(input1, input2, logic, oldValue, parentContainer) {
+    let result = "";
+    if (logic === "and") {
+        result = input1 === "1" && input2 === "1" ? "1" : "0";
+    } else if (logic === "or") {
+        result = input1 === "0" && input2 === "0" ? "0" : "1";
+    }
+
+    if (result !== oldValue) {
+        toggleLight(parentContainer);
+    }
+
+
+    console.log(`${input1}と${input2}の${logic}をとります。結果は${result}です。前の値は${oldValue}です`)
 }
