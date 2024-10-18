f7d0c1fad1526848b83c1d32c29f33bb58c70ef5
ro026 <o.r1997ti@gmail.com>
2024-10-18 12:48:14 +0900

    第１５回資料更新

--- top.js
+++ top.js
@@ -1,6 +1,11 @@
 (function () {
     const updateList = [
         {
+            date: "2024/10/18",
+            kind: "add",
+            text: "[情報I] 第15回の資料を公開しました"
+        },
+        {
             date: "2024/10/11",
             kind: "add",
             text: "[情報I] 第13回・第14回の資料を公開しました"
