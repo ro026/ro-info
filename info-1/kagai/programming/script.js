5893c67973eebef992d42d41c3091f9343c9b951
ro026 <o.r1997ti@gmail.com>
2024-10-24 10:06:25 +0900

    XTetraで確認するボタンのスタイルと文言を修正

--- info-1/kagai/programming/script.js
+++ info-1/kagai/programming/script.js
@@ -2,7 +2,7 @@
     const div = document.createElement('div');
     let innerHTML = "";
     xtertaList.map((list, index) => {
-        innerHTML += `<h4><span class="radius backgroundColorBlue"}><a class="textDecorationNone" href="${list.link}" style="font-size: 14px" target="_blank">XTertaで確認する</a></span></h4>`;
+        innerHTML += `<h4><span class="radius backgroundColorBlue"}><a class="textDecorationNone" href="${list.link}" style="font-size: 14px" target="_blank">↓のプログラムをXTertaで確認する</a></span></h4>`;
         innerHTML += `<pre style="background: white; border: 1px solid black; padding: 3px 6px">${list.code}</pre>`;
         if (list.option) {
             innerHTML += `<pre>${list.option}</pre>`
