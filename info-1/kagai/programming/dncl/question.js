c4a27a9bc0fd2fdaf43bb9f18bdbff1dda3eff67
okaryoh <okatyan1997@gmail.com>
2024-12-06 12:27:18 +0900

    仮でスプレッドシートに送信できるようにする

--- info-1/kagai/programming/dncl/question.js
+++ info-1/kagai/programming/dncl/question.js
@@ -133,6 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
 ///
 ///
 document.getElementById("create-url-button").addEventListener("click", function () {
+    sendData();
     const openTsuchinokoButton = document.getElementById("open-tsuchinoko-button");
     const closeButton = document.getElementById("close-button");
 
@@ -207,4 +208,29 @@ function addEventListenersToInputs() {
             this.value = correctCodeStyle(this.value)
         });
     });
+}
+
+async function sendData() {
+    const scriptURL = "https://script.google.com/macros/s/AKfycbxSRGHtCL50KExeT6pZhJkwbrpGr2sViBW-bKPtBdIOn0Faz-rZ5ADXG2_kzi14KPSV-Q/exec";
+    const inputs = document.querySelectorAll('input')
+    const data = {
+        name: num,
+        email: level,
+        message: Array.from(inputs).map(input => input.value).join(','),
+    };
+
+    try {
+        const response = await fetch(scriptURL, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const result = await response.json();
+        // alert(result.status === 'success' ? '送信成功！' : '送信失敗：' + result.message);
+    } catch (error) {
+        // alert('エラーが発生しました: ' + error.message);
+    }
 }
