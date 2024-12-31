44414050b55d8ac1dbcde8a5dee59467c7050ba4
okaryoh <okatyan1997@gmail.com>
2025-01-01 01:05:12 +0900

    情報の課題について更新を行う

--- top.js
+++ top.js
@@ -1,6 +1,11 @@
 (function () {
     const updateList = [
         {
+            date: "2025/1/1",
+            kind: "update",
+            text: "[情報I] 冬休み課題のやり方を追加しました"
+        },
+        {
             date: "2024/12/25",
             kind: "add",
             text: "[情報I] 冬休みの課題の情報を追加しました"
